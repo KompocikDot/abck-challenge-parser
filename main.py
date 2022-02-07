e9e8fa564c3332a276de08a9553a08568f7d5a44
unknown <stigpolski@gmail.com>
2022-02-07 21:04:04 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from parser import Parser
+
+def main():
+    Parser()
+
+if __name__ == "__main__":
+    main()
---
+++ parser.py
@@ -0,0 +1,58 @@
+"""Simple Akamai ABCK challenge parser"""
+from os import system
+from colorama import init, Fore
+init(autoreset=True)
+
+class Parser:
+    """ABCK Parser class"""
+    def __init__(self) -> None:
+        self.headers = ["mn_al", "mn_tcl", "mn_il", "mn_lg"]
+        self.logic()
+
+
+    def logic(self) -> None:
+        """Its just logic of whole module making constructor more readable"""
+        try:
+            self.mode = int(input("Which mode do you want to use? \n[1 - one] [2 - compare] "))
+        except ValueError:
+            self.mode = 1
+
+        if self.mode == 1:
+            self.challenge = input("Paste bmak[mn_r] here: ").strip()
+        else:
+            self.challenge = (input("Paste bmak[mn_r] here: ").strip(), input("Paste bmak[mn_r] here: ").strip())
+
+        if self.challenge != "":
+            if isinstance(self.challenge, tuple):
+                changes = []
+                for challenge_str in self.challenge:
+                    changes.append(self.undo_mnpr(challenge_str))
+                self.compare_mode(changes)
+
+            else:
+                self.undo_mnpr(self.challenge)
+
+
+    def clear_console(self) -> None:
+        """Clears console"""
+        system('cls')
+
+
+    def undo_mnpr(self, abck_string: str) -> list[tuple] | None:
+        """Destructs challenge string into smaller pieces"""
+        mn_pr = self.challenge.split(";")
+        self.clear_console()
+
+        zipped = zip(self.headers, mn_pr)
+        if self.mode == 2:
+            return zipped
+
+        for zipped_item in zipped:
+            print(Fore.BLUE + f"{zipped_item[0]}" + Fore.RESET, f": {zipped_item[1]}")
+
+    def compare_mode(self, changes: list) -> None:
+        """Prints changes between two challenge strings"""
+        print(changes)
+
+
+Parser()
