2cf1bc6e4892c2a80075f885ff339e1fd00366ee
unknown <stigpolski@gmail.com>
2022-02-08 17:22:52 +0100

    Removed few things and added mn_lg checking

--- challenge.py
+++ challenge.py
@@ -9,12 +9,13 @@ class Challenge:
         self.headers = ["mn_al", "mn_tcl", "mn_il", "mn_lg"]
         self.mnlg_headers = [
             "mn_abck", "mn_rts", "mn_psn", "mn_cc", "mn_cd",
-            "mn_cd + mn_mc_indx", "rand_toString(16)", "mn_cc + rand_toString(16)",
+            "m = mn_cd + mn_mc_indx", "n = rand_toString(16)", "mn_cc + m + n",
             "mn_s(r)", "mn_rt", "mn_wt", "get_cf_date()",
         ]
 
         self.detect_sensor()
         self.destruct_str()
+        self.check_mnlg()
         self.print_deconstructed()
 
 
@@ -33,15 +34,34 @@ class Challenge:
         self.deconstructed_mnlg = self.deconstructed[-1].split(",")
         self.deconstructed_mnlg = self.deconstructed_mnlg[:8] + \
             [",".join(self.deconstructed_mnlg[8:-3])] + self.deconstructed_mnlg[-3:]
-        self.deconstructed_mnlg = zip(self.mnlg_headers, self.deconstructed_mnlg)
-        self.deconstructed = zip(self.headers, self.deconstructed)
 
 
     def print_deconstructed(self) -> None:
         """Pretty prints deconstructed challenge"""
-        for item in self.deconstructed:
+
+        for item in zip(self.headers, self.deconstructed):
             print(Fore.CYAN + item[0] + Fore.RESET + ": " + item[1])
 
         print(Fore.GREEN + "mn_lg ingredients" + Fore.RESET)
-        for mnlg_item in self.deconstructed_mnlg:
-            print(Fore.CYAN + mnlg_item[0] + Fore.RESET + ": " + mnlg_item[1])
+        for mnlg_item in zip(self.mnlg_headers, self.deconstructed_mnlg):
+            if mnlg_item[1][1]:
+                color = Fore.GREEN
+            else:
+                color = Fore.RED
+
+            print(Fore.CYAN + mnlg_item[0] + ": " + color + mnlg_item[1][0] + Fore.RESET)
+
+    def check_mnlg(self) -> None:
+        """Checks if values inside mn_lg are correct"""
+        to_zip = [True] * 12
+        if self.deconstructed_mnlg[3] != "".join(self.deconstructed_mnlg[:3]):
+            to_zip[3] = False
+        if self.deconstructed_mnlg[4] != self.deconstructed_mnlg[5]:
+            to_zip[4], to_zip[5] = False, False
+        if self.deconstructed_mnlg[7] != self.deconstructed_mnlg[3] + self.deconstructed_mnlg[5] \
+            + self.deconstructed_mnlg[6]:
+            to_zip[7] = False
+        if int(self.deconstructed_mnlg[-1]) < int(self.deconstructed_mnlg[1]):
+            to_zip[11] = False
+
+        self.deconstructed_mnlg = zip(self.deconstructed_mnlg, to_zip)
